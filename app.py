8402171ac8cff287f957030b1426d4693f58ea79
githubwhip <y0unxxdme@gmail.com>
2024-12-15 20:34:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,10 @@ if submit_button:
                 my_bar.progress(percent_complete + 1, text=progress_text)
             time.sleep(1)
             my_bar.empty()
-            st.switch_page("pages/car_enrollment_data.py")
+            if st.button("등록 현황 학습하러 가기"):
+                st.switch_page("pages/car_enrollment_data.py")
+            if st.button("판매 현황 학습하러 가기"):
+                st.switch_page("pages/car_sales_data.py")
 
         else:
             st.error("아이디 또는 비밀번호가 일치하지 않습니다.")
