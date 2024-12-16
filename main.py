0f9d89fb2a8bd18d91357746ec937df6c67c6a1f
githubwhip <y0unxxdme@gmail.com>
2024-12-16 15:27:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,27 +18,19 @@ if submit_button:
         st.warning("ID와 비밀번호를 모두 입력해주세요.")
     else:
         # 사용자 확인
-        user = data[(data["ID"] == ID) & (data["PW"] == str(PW))]
+        class = data[(data["ID"] == ID) & (data["PW"] == str(PW))]
         
         if not user.empty:
-            st.success(f"{ID}님 환영합니다!")
+            st.success(f"{ID}선생님의 반 학생 여러분, 환영합니다!")
             st.session_state["authenticated"]=True
             st.session_state["ID"] = ID
             
-            progress_text = "로그인 중입니다."
+            progress_text = "접속 중입니다."
             my_bar = st.progress(0, text=progress_text)
             for percent_complete in range(100):
                 time.sleep(0.01)
                 my_bar.progress(percent_complete + 1, text=progress_text)
             time.sleep(1)
             my_bar.empty()
-            
-            col1, col2 = st.columns(2)
-            with col1:
-                if st.button("등록 현황 학습", key="enrollment"):
-                    st.switch_page("pages/car_enrollment_data.py")
-            with col2:
-                if st.button("판매 현황 학습", key="sales"):
-                    st.switch_page("pages/car_sales_data.py")
         else:
-            st.error("아이디 또는 비밀번호가 일치하지 않습니다.")
+            st.error("접속 정보가 일치하지 않습니다.")
