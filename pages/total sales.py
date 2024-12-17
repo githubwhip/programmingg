e8460b1cc29c9617097ae752cd50353f234c8fc2
githubwhip <y0unxxdme@gmail.com>
2024-12-17 16:08:00 +0900

    Update total sales.py

--- pages/total sales.py
+++ pages/total sales.py
@@ -1,53 +1,156 @@
 import streamlit as st
-import pandas as pd
-import folium
-from streamlit_folium import folium_static
-from folium.plugins import MarkerCluster
+import tiktoken
+from loguru import logger
 
-# 데이터프레임 생성
-df = pd.DataFrame({
-    '지역': ['서울', '경기', '인천', '강원', '충북', '충남', '대전', '세종', '경북', '경남', '부산', '대구', '울산', '전북', '전남', '광주', '제주'],
-    '위도': [37.5665, 37.4138, 37.4563, 37.8228, 36.6357, 36.6588, 36.3504, 36.4800, 36.0190, 35.2383, 35.1796, 35.8714, 35.5384, 35.8242, 34.8161, 35.1595, 33.4890],
-    '경도': [126.9780, 127.5183, 126.7052, 128.1555, 127.4914, 126.6728, 127.3845, 127.2890, 129.3434, 128.6919, 129.0756, 128.6014, 129.3114, 127.1480, 126.4631, 126.8526, 126.4983],
-    '합계': [63807, 90624, 30905, 15728, 17511, 20225, 15564, 3562, 23023, 27147, 27551, 26691, 6207, 16256, 19966, 10303, 35619]
-})
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
-# 지도 생성
-# 지도 생성
-m = folium.Map(location=[36.5, 127.5], zoom_start=7)
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
 
-# 마커 클러스터 생성
-marker_cluster = MarkerCluster().add_to(m)
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
 
-# 원형 마커와 텍스트 추가
-for idx, row in df.iterrows():
-    # 숫자를 천 단위 구분자로 포맷팅
-    formatted_number = format(row['합계'], ',')
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+
+# from streamlit_chat import message
+from langchain.callbacks import get_openai_callback
+from langchain.memory import StreamlitChatMessageHistory
+
+def main():
+    st.set_page_config(
+    page_title="DirChat",
+    page_icon=":books:")
+
+    st.title("_Private Data :red[QA Chat]_ :books:")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+
+    with st.sidebar:
+        uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
+        openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+        process = st.button("Process")
+    if process:
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+        files_text = get_text(uploaded_files)
+        text_chunks = get_text_chunks(files_text)
+        vetorestore = get_vectorstore(text_chunks)
+     
+        st.session_state.conversation = get_conversation_chain(vetorestore,openai_api_key) 
+
+        st.session_state.processComplete = True
+
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [{"role": "assistant", 
+                                        "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    history = StreamlitChatMessageHistory(key="chat_messages")
+
+    # Chat logic
+    if query := st.chat_input("질문을 입력해주세요."):
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        with st.chat_message("assistant"):
+            chain = st.session_state.conversation
+
+            with st.spinner("Thinking..."):
+                result = chain({"question": query})
+                with get_openai_callback() as cb:
+                    st.session_state.chat_history = result['chat_history']
+                response = result['answer']
+                source_documents = result['source_documents']
+
+                st.markdown(response)
+                with st.expander("참고 문서 확인"):
+                    st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
+                    st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
+                    st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
+                    
+
+
+# Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+def tiktoken_len(text):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+def get_text(docs):
+
+    doc_list = []
     
-    folium.Circle(
-        location=[row['위도'], row['경도']],
-        radius=row['합계']/100,
-        color='blue',
-        fill=True,
-        fill_color='blue',
-        fill_opacity=0.4
-    ).add_to(marker_cluster)
-    
-    # HTML에서 텍스트 크기를 키우고 포맷팅된 숫자 사용
-    html_content = f"""
-        <div style='font-size: 14pt; font-weight: bold; text-align: center;'>
-            {row['지역']}<br>{formatted_number}
-        </div>
-    """
-    
-    folium.map.Marker(
-        [row['위도'], row['경도']],
-        icon=folium.DivIcon(
-            html=html_content,
-            icon_size=(150,50),
-            icon_anchor=(75,25)
+    for doc in docs:
+        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
+        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
+            file.write(doc.getvalue())
+            logger.info(f"Uploaded {file_name}")
+        if '.pdf' in doc.name:
+            loader = PyPDFLoader(file_name)
+            documents = loader.load_and_split()
+        elif '.docx' in doc.name:
+            loader = Docx2txtLoader(file_name)
+            documents = loader.load_and_split()
+        elif '.pptx' in doc.name:
+            loader = UnstructuredPowerPointLoader(file_name)
+            documents = loader.load_and_split()
+
+        doc_list.extend(documents)
+    return doc_list
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=900,
+        chunk_overlap=100,
+        length_function=tiktoken_len
+    )
+    chunks = text_splitter.split_documents(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+                                        model_name="jhgan/ko-sroberta-multitask",
+                                        model_kwargs={'device': 'cpu'},
+                                        encode_kwargs={'normalize_embeddings': True}
+                                        )  
+    vectordb = FAISS.from_documents(text_chunks, embeddings)
+    return vectordb
+
+def get_conversation_chain(vetorestore,openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm, 
+            chain_type="stuff", 
+            retriever=vetorestore.as_retriever(search_type = 'mmr', vervose = True), 
+            memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+            get_chat_history=lambda h: h,
+            return_source_documents=True,
+            verbose = True
         )
-    ).add_to(marker_cluster)
 
-st.title('지역별 자동차 등록 현황')
-folium_static(m)
+    return conversation_chain
+
+
+
+if __name__ == '__main__':
+    main()
